74dab3397ef7242f939013bebf96045900ca4070
Nicolas Minguet <nicolas.minguet@hotmail.fr>
2019-05-31 15:03:04 +0200

    button top doesnt appear before scroll anymore

--- app/javascript/components/topbutton.js
+++ app/javascript/components/topbutton.js
@@ -11,7 +11,6 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  console.log('super ça marche');
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
