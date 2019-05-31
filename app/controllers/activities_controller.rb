9543638a348aef35263a06001bbd444544bfe874
Emilie Murat <thekwette@gmail.com>
2019-05-31 13:00:01 +0200

    destroy method for user activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -42,6 +42,12 @@ class ActivitiesController < ApplicationController
     @review = Review.new
   end
 
+  def destroy
+    @activity = Activity.find(params[:id])
+    @activity.destroy
+    redirect_to activities_path
+  end
+
   private
 
   def activity_params
